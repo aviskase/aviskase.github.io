343bd764308641a9a1fefe68094df1d5477f4fab
Yuliya Bagriy <aviskase@gmail.com>
2016-05-02 01:48:53 +0600

    Refactor shell script

--- .test_size.sh
+++ .test_size.sh
@@ -1,46 +1,45 @@
 #!/bin/bash
 
-check_size()
-{
-    for FILENAME in $DIR/*.png  #traverse filenames...
-
-        do
-        FILESIZE=$(stat -c%s "$FILENAME")
-
-            if [ $FILESIZE -gt $BORDER_SIZE_IN_BYTES ]
-            then
-                FILESIZE_IN_MB=$((($FILESIZE / 1024) / 1024))
-                echo "ERROR: $FILENAME is too large = $FILESIZE_IN_MB MB."
-                STATUS=2
-            fi
-
-        done
-}
-
-
-
 DIR=$1
 BORDER_SIZE=$2
 
-STATUS=0
-
-if [[ -n "$DIR" ]]; then
-    if [[ -n "$BORDER_SIZE" ]]; then
-        BORDER_SIZE_IN_BYTES=$(($BORDER_SIZE*1024*1024))
-        check_size
-    else
-        echo "ERROR: No size specified"
-        STATUS=1
-    fi
-else
+if [ ! -n "$DIR" ]; then
     echo "ERROR: No dir specified"
-    STATUS=1
+    exit 1
 fi
 
-if [[ "$STATUS" == "0" ]]; then
+if [ ! -d "$DIR" ]; then
+    echo "ERROR: No such dir present"
+    exit 1
+fi
+
+if [ ! -n "$BORDER_SIZE" ]; then
+    echo "ERROR: No size specified"
+    exit 1
+fi
+
+if [ ! "$(ls -A $DIR)" ]; then
+    echo "INFO: Dir is empty"
+    exit 0
+fi
+
+BORDER_SIZE_IN_BYTES=$(($BORDER_SIZE*1024*1024))
+STATUS=0
+for FILENAME in $DIR/*  #traverse filenames...
+do
+    FILESIZE=$(stat -c%s "$FILENAME")
+
+    if [ $FILESIZE -gt $BORDER_SIZE_IN_BYTES ]; then
+        FILESIZE_IN_MB=$((($FILESIZE / 1024) / 1024))
+        echo "ERROR: $FILENAME is too large = $FILESIZE_IN_MB MB."
+        STATUS=2
+    fi
+done
+
+if [ "$STATUS" -eq "0" ]; then
     echo "SUCCESS: All files are under $BORDER_SIZE MB."
+    exit 0
+else
+    exit 2
 fi
-
-
-exit $STATUS
 
