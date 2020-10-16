ce5c2a91a06d723b0e4ab15f0091e697a17e0bed
Yuliya Bagriy <aviskase@gmail.com>
2020-10-15 23:43:27 -0400

    closes #48

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,10 +1,12 @@
 const themeDir = __dirname + '/../../';
 
-module.exports = {    
-    plugins: [        
+module.exports = {
+    plugins: [
         require('postcss-import')({
             path: [themeDir]
-            }), 
+        }), 
+        require('postcss-simple-vars'),
+        require('postcss-nested'),
         require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
         require('autoprefixer')({
             path: [themeDir]
