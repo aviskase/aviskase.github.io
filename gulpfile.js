94e299297e96e6c10a238fd45c91a3e82568c804
Yuliya Bagriy <aviskase@gmail.com>
2020-10-29 21:00:07 -0400

    fixes #56

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const shell = require('gulp-shell');
+const del = require('del')
+const webp = require('gulp-webp');
+
+gulp.task('hugoServe', shell.task('hugo server --watch --i18n-warnings --path-warnings --disableFastRender', {
+    env: { MODE: 'development', ...process.env }
+}));
+
+gulp.task('hugoBuild', shell.task('hugo -e "production" --minify --debug --i18n-warnings --path-warnings', {
+    env: { MODE: 'production', 'NODE_ENV': 'production', ...process.env }
+}));
+
+function cleanOutput() {
+    return del(['./output/']);
+}
+
+function convertToWebp() {
+    const base = './content/articles';
+    return gulp.src(`${base}/**/*.{jpg,jpeg,png,svg}`, { base: '.', ignore: [`${base}/**/feature.png`] })
+        .pipe(webp())
+        .pipe(gulp.dest('./'));
+}
+
+const watch = gulp.series('hugoServe');
+const build = gulp.series(cleanOutput, convertToWebp,'hugoBuild');
+
+
+// expose tasks to CLI
+exports.watch = watch;
+exports.build = build;
+exports.default = watch;
