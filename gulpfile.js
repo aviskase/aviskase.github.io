1312f1cb0768e97b466405bd77c6fb4ae1ce62f4
Yuliya Bagriy <aviskase@gmail.com>
2021-05-30 22:25:12 -0400

    feat: switch to hugo native webp processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const shell = require('gulp-shell');
 const del = require('del')
-const webp = require('gulp-webp');
 
 gulp.task('hugoServe', shell.task('hugo server -D --watch --i18n-warnings --path-warnings --disableFastRender', {
     env: { MODE: 'development', ...process.env }
@@ -15,15 +14,8 @@ function cleanOutput() {
     return del(['./output/']);
 }
 
-function convertToWebp() {
-    const base = './content/articles';
-    return gulp.src(`${base}/**/*.{jpg,jpeg,png,svg}`, { base: '.', ignore: [`${base}/**/feature.png`] })
-        .pipe(webp())
-        .pipe(gulp.dest('./'));
-}
-
 const watch = gulp.series('hugoServe');
-const build = gulp.series(cleanOutput, convertToWebp,'hugoBuild');
+const build = gulp.series(cleanOutput,'hugoBuild');
 
 
 // expose tasks to CLI
