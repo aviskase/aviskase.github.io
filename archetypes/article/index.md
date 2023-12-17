22d2e8dae0c778508b3b6583fc0db0a5ff453123
Yuliya Bagriy <aviskase@gmail.com>
2023-12-16 21:42:53 -0500

    better add draft task handler
    also fixes #76

--- archetypes/article/index.md
+++ archetypes/article/index.md
@@ -1,6 +1,11 @@
 ---
-title: "{{ replace (replace .Name "-" " ") (now.Format "2006 01 02 ") "" | title }}"
 date: {{ .Date }}
+{{- $title := getenv "HUGO_TITLE" }}
+{{- if $title }}
+title: "{{ $title }}"
+{{- else }}
+title: "{{ replace (replace .Name "-" " ") (now.Format "2006 01 02 ") "" | title }}"
+{{- end }}
 draft: true
 see_also: []
 ---
--- invoke.js
+++ invoke.js
@@ -2,20 +2,36 @@
 
 const { execFile } = require('child_process');
 const { program } = require('commander');
+var slug = require('slug')
 
 program
   .command('draft')
   .description('add a new draft article')
   .requiredOption('-t, --title <article_title>', 'article title')
   .action((opts) => {
-        const title = opts.title.toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,'');
-        const now = new Date();
-        const draftPath = `articles/${now.getFullYear()}/${now.toLocaleDateString('en-CA')}-${title}`;
-
-        execFile('hugo', ['new', '--kind', 'article', draftPath], (error, stdout, stderr) => {
-            if (error) { throw error; }
-            console.log(stdout);
-        });
+    if (!opts.title || opts.title.trim() === '') {
+      console.error('No title provided');
+      process.exitCode = 1;
+      return;
+    }
+    const title = slug(opts.title);
+    const now = new Date();
+    const draftPath = `articles/${now.getFullYear()}/${now.toLocaleDateString('en-CA')}-${title}`;
+    execFile(
+      'hugo', 
+      ['new', '--kind', 'article', draftPath], 
+      { env: { ...process.env, HUGO_TITLE: opts.title } },
+      (error, stdout, stderr) => {
+        if (error || stderr) { 
+          if (error?.message) { console.error(error?.message) };
+          if (stderr) { console.error(stderr) }; 
+          if (stdout) { console.error(stdout) }; 
+          process.exitCode = 1;
+          return; 
+        }
+        console.log(stdout);
+      }
+    );
 });
 
 program.parse(process.argv);
